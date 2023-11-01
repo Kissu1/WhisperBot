2ff9105d375bf0cc4e7a4459878045f8db4a8fd8
Kissu <142857872+Kissu1@users.noreply.github.com>
2023-11-01 11:09:43 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -7,14 +7,13 @@ class Data:
 Hey {}.
 Welcome to {}
 
-I am the Master of Whisperers (like Varys in Game of Thrones).
-
+I am the Master of Whisperers 
 You can use me to send whispers to your friend in groups and channels (even if I'm not there).
 Only that friend and you will be able to read the message even though others are in same group. 
 
 To see how to use me press 'How to Use' below.
 
-By @StarkBots
+By @kissu123456
     """
 
     # Home Button
@@ -31,30 +30,25 @@ By @StarkBots
             InlineKeyboardButton("How to Use ❔", callback_data="help"),
             InlineKeyboardButton("🎪 About 🎪", callback_data="about")
         ],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")],
-        [InlineKeyboardButton("🎨 Support Group 🎨", url="https://t.me/StarkBotsChat")],
+        [InlineKeyboardButton(" 𝐊𝐢𝐬𝐬𝐮 💞 ", url="https://t.me/kissu123456")],
+        [InlineKeyboardButton("🎨 Movie Bot 🎨", url="https://t.me/Kissu_movies_bot")],
     ]
 
     # Help Message
     HELP = """
 Just type the message in below format in any chat.
 
-`@WhisperStarkBot your_message friend_username/id`
+`@WhisperkissBot your_message friend_username/id`
     """
 
     # About Message
     ABOUT = """
 **About This Bot** 
 
-Bot created by @StarkBots
+Bot created by @kissi123456
 
-Source Code : [Click Here](https://github.com/StarkBotsIndustries/WhisperBot)
-
-Inspired By : nnbbot
 
 Framework : [Pyrogram](docs.pyrogram.org)
 
 Language : [Python](www.python.org)
-
-Developer : @StarkProgrammer
     """
